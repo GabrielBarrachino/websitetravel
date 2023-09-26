6724e333e91e4f381f16af6d1e5541296b3c9dca
GabrielBarrachino <gabrielbarrachino@outlook.com>
2023-09-26 15:54:10 -0300

    Home mobile feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import '../src/Styles/css/main.min.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { Home } from './Components/Home/Home';
 // import { About } from './Components/About/About';
 // import { Blog } from './Components/Blog/Blog';
 // import { Footer } from './Components/Footer/Footer';
-// import { Home } from './Components/Home/Home';
 // import { Offers } from './Components/Offers/Offers';
 // import { Popular } from './Components/Popular/Popular';
 
@@ -11,8 +11,8 @@ export function App() {
   return (
     <div>
       <Navbar/>
-      {/* <Home/>
-      <Popular/>
+      <Home/>
+      {/* <Popular/>
       <Offers/>
       <About/>
       <Blog/>
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,7 +1,42 @@
 export function Home() {
     return (
       <div>
-        <h1>Home</h1>
+        <section className="home" id="home">
+          <div className="secContainer container">
+            <div className="homeText">
+              <div className="homeText">
+                <h1 className="title">Plan Your Trip With Travel Dot</h1>
+                <p className="subtitle">
+                  Travel to your favourite city with respctful of the environment!
+                </p>
+
+                <button className="btn">
+                  <a href="#">Explore Now</a>
+                </button>
+              </div>
+            </div>
+
+            <form action="" className="homeCard grid">
+              <div className="locationDiv">
+                <label htmlFor="location">Location</label>
+                <input type="text" placeholder="Dream Destination" required/>
+              </div>
+
+              <div className="distDiv">
+                <label htmlFor="distance">Distance</label>
+                <input type="text" placeholder="11/Meters" required/>
+              </div>
+
+              <div className="priceDiv">
+                <label htmlFor="price">Price</label>
+                <input type="number"  min="140" max="500" placeholder="$140-$500" required/>
+              </div>
+              <button className="btn">
+                Search
+              </button>
+            </form>
+          </div>
+        </section>
       </div>
     )
   }
